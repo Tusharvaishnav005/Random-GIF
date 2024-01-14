c9ffe604c6b9fbca1d56075c64300503f0bd808a
Tushar Vaishnav <135534368+Tusharvaishnav005@users.noreply.github.com>
2024-01-15 02:09:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
        px-10 py-2 text-4xl font-bold ">
       RANDOM GIFS</h1>
       <div className="flex flex-col w-full items-center gap-y-10 mt-[30px]">
+        
         <Random />
         <Tag />
       </div>
